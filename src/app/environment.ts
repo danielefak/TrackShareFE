d9593a90a3c3502ae7c84f61280afcb8972702dc
danielefak <daniele.fak@gmail.com>
2026-05-17 15:21:51 +0200

    Connecting to real backend

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,8 +1,9 @@
 import { InjectionToken } from '@angular/core';
+import { environment } from '../environments/environment';
 
 export const ENVIRONMENT = new InjectionToken<{ apiUrl: string }>('environment', {
   providedIn: 'root',
   factory: () => ({
-    apiUrl: 'http://127.0.0.1:8000' // Change as needed
+    apiUrl: environment.apiUrl
   })
 });
